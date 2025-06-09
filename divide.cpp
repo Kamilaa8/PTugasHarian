9ad74e0aee858599cc26cd49d944e38ecbb1adb8
nawuraa <nauraazura33@gmail.com>
2025-06-09 18:27:05 +0700

    divide.cpp

--- divide.cpp
+++ divide.cpp
@@ -1,3 +1,55 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
+
+int main() {
+    int n;
+    cout << "Masukkan jumlah tugas: ";
+    cin >> n;
+    cin.ignore(); 
+    vector<string> urgent, medium, low;
+
+    for (int i = 0; i < n; ++i) {
+        string nama;
+        int hari;
+
+        cout << "Tugas ke-" << i + 1 << endl;
+        cout << "Nama tugas: ";
+        getline(cin, nama);
+        cout << "Berapa hari lagi menuju deadline? ";
+        cin >> hari;
+        cin.ignore();
+
+      
+        if (hari <= 2) {
+            urgent.push_back(nama);
+        } else if (hari <= 5) {
+            medium.push_back(nama);
+        } else {
+            low.push_back(nama);
+        }
+    }
+
+
+    cout << "Hasil Pembagian Tugas Berdasarkan Urgensi" << endl;
+
+    cout << "Urgent - ≤ 2 hari" << endl;
+    for (string& t : urgent) {
+        cout << "- " << t << endl;
+    }
+
+    cout << "[Medium - 3 sampai 5 hari]";
+    for (string& t : medium) {
+        cout << "- " << t << endl;
+    }
+
+    cout << "[Low - > 5 hari]";
+    for (string& t : low) {
+        cout << "- " << t << endl;
+    }
+    
+    return 0;
+}
 
