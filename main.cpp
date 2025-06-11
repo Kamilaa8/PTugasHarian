987f3b003dc1ff64f201a6ad67d17fd71e8dc30a
nawuraa <nauraazura33@gmail.com>
2025-06-11 12:05:31 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
+
+struct Tugas {
+    string nama;
+    int hariMenujuDeadline;
+    int prioritas;
+};
+
+vector<Tugas> divideInput(int jumlah) {
+    vector<Tugas> daftarTugas;
+    cin.ignore(); 
+    for (int i = 0; i < jumlah; i++) {
+        Tugas t;
+        cout << "\nMasukkan nama tugas ke-" << i + 1 << ": ";
+        getline(cin, t.nama);
+
+        cout << "Masukkan jumlah hari menuju deadline: ";
+        cin >> t.hariMenujuDeadline;
+
+        cout << "Masukkan prioritas (1 = tinggi, 2 = sedang, 3 = rendah): ";
+        cin >> t.prioritas;
+        cin.ignore();
+
+        daftarTugas.push_back(t);
+    }
+    return daftarTugas;
+}
+
 
